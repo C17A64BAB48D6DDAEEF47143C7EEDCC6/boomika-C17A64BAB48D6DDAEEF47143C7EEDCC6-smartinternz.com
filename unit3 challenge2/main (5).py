baed0a58d531b65438081fbf7e177fa9f0c95acd
C17A64BAB48D6DDAEEF47143C7EEDCC6 <148187430+C17A64BAB48D6DDAEEF47143C7EEDCC6@users.noreply.github.com>
2023-10-17 11:43:47 +0530

    Add files via upload

---
+++ unit3 challenge2/main (5).py
@@ -0,0 +1,14 @@
+def CgpaCslc(marks,n):
+  grade = [0] * n
+  sum = 0
+  for i in range(n):
+       grade[i] = (marks,[i] / 10)
+  for i i in range(n):
+      sum += grade [i]
+  cgps = sum / n
+  return chpa
+n = 5
+marks = [90,80,70,80,90]
+cgpa = Cgpacalx(marks,n)
+print("CGPA ="'%.1f' %(cgpa))
+print("CGPApercrntage"￼gpaCslclclclclclclclcGPA =",cgpa))
